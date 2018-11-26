7fe61ac02c8c2b772c7583b6e342f52e664599db
atkinsonholly <atkinsonholly@gmail.com>
2018-11-26 17:57:03 +0000

    Tests passing

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,17 @@
 # Your code goes here!
+require 'pry'
+class Anagram
+
+  attr_accessor :word
+
+  def initialize (word)
+    @word = word
+  end
+
+  def match(comparison_words) #["hello", "world", "zombies", "pants", "dipper"]
+    array = comparison_words.select {|word| word.chars.sort.join == self.word.chars.sort.join}
+
+  end
+
+
+end
